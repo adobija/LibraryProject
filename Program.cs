21baa0cf4d05e9c3851d7c3c59b883aa59237e57
Aleksander Dobija <aledob1324@gmail.com>
2025-01-07 18:15:50 +0100

    Add to remote repo source files

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
