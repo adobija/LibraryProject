c4dd6c9c92e53224286c0685da7c0da078a79064
Aleksander Dobija <aledob1324@gmail.com>
2025-01-07 18:35:12 +0100

    Create book abstract class

---
+++ abstracts/BookAbstract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.abstracts
+{
+    internal abstract class BookAbstract
+    {
+        private string Title;
+        private string Author;
+        private int ISBN;
+
+    }
+}
