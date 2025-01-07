50b5d7b1fca9184211585fd62f378bc5ddd3d1ac
Aleksander Dobija <aledob1324@gmail.com>
2025-01-07 20:02:41 +0100

    Test of created code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace LibraryProject
+﻿using LibraryProject.classes;
+
+namespace LibraryProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            FantasyBook wiedzmin = new FantasyBook();
+
+            wiedzmin.printDetailsOfBook();
+
+            RomanceNovelBook nudy = new RomanceNovelBook("Jakis tytul", "autor", 1235438370384);
+
+            nudy.printDetailsOfBook();
+
         }
     }
 }
