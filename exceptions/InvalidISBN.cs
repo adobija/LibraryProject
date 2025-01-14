e86fdf3cba6e780de521a456b7670551c1668a02
Aleksander Dobija <aledob1324@gmail.com>
2025-01-14 22:55:46 +0100

    Create custom exception class

---
+++ exceptions/InvalidISBN.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.exceptions
+{
+    internal class InvalidISBN : Exception
+    {
+        public InvalidISBN() : base("Provided invalid ISBN number") { 
+            
+        }
+
+        public InvalidISBN(string message) : base(message)
+        {
+
+        }
+    }
+}
