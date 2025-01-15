56e52ce644bc1bf2c5624e5f7d382b35b43ced00
Aleksander Dobija <aledob1324@gmail.com>
2025-01-15 22:57:02 +0100

    Create helping class to fetch list of users from DB

---
+++ libraries/RefactorUserDb.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryProject.classes;
+
+namespace LibraryProject.libraries
+{
+    internal class RefactorUserDb
+    {
+        public static List<User> FetchUsers()
+        {
+            string pathToTxt = "../../../libraries/UserDB.txt";
+            List<User> users = new List<User>();
+            try
+            {
+                using (StreamReader usersDB = new StreamReader(pathToTxt))
+                {
+                    while (usersDB.Peek() >= 0)
+                    {
+                        string[] line = usersDB.ReadLine().Split(",");
+                        User user = new User(line[0], line[1], int.Parse(line[2]));
+                        users.Add(user);
+                    }
+
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("UserController fail: UserDB.txt file not found");
+            }
+
+            return users;
+        }
+    }
+}
