9df102137983b8649e1c1419cc140fc64330b7b8
Aleksander Dobija <aledob1324@gmail.com>
2025-01-15 22:53:01 +0100

    Create User class

---
+++ classes/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryProject.abstracts;
+using LibraryProject.controllers;
+using LibraryProject.exceptions;
+
+namespace LibraryProject.classes
+{
+    internal class User : UserAbstract
+    {
+        public User(string userName, string password) : base(userName, password) {
+        }
+
+        public User(string userName, string password, int id) : base(userName, password, id)
+        {
+        }
+
+
+    }
+}
