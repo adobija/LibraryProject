c8b60c4b3415ef14740113564078ca576698f90e
Aleksander Dobija <aledob1324@gmail.com>
2025-01-15 22:54:52 +0100

    Create custom exception for taken username

---
+++ exceptions/UserAlreadyExistException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.exceptions
+{
+    internal class UserAlreadyExistException : Exception
+    {
+
+        public UserAlreadyExistException() : base("User with this username already exist!")
+        {
+
+        }
+
+        public UserAlreadyExistException(string message) : base(message)
+        {
+
+        }
+    }
+}
