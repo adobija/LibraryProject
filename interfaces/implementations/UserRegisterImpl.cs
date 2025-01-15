1423b3e8548f212d75c963258080cc372a0ad12e
Aleksander Dobija <aledob1324@gmail.com>
2025-01-15 22:56:02 +0100

    Create implementation of UserRegister interface

---
+++ interfaces/implementations/UserRegisterImpl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryProject.classes;
+
+namespace LibraryProject.interfaces.implementations
+{
+    internal class UserRegisterImpl :UserRegister
+    {
+        public void RegisterUser(string userName, string password)
+        {
+            User user = new User(userName, password);
+
+            string pathToUserDB = "../../../libraries/UserDB.txt";
+            try
+            {
+                using (StreamWriter file = new StreamWriter(pathToUserDB, true)) {
+                    file.WriteLine(user.ToString());
+                }
+                Success();
+            }
+            catch (FileNotFoundException e)
+            { 
+            Console.WriteLine("UserRegisterImpl fail: file UserDB.txt not found");
+            }
+        }
+
+        public void Success()
+        {
+            Console.WriteLine("Successfully registered!");
+        }
+
+    }
+}
