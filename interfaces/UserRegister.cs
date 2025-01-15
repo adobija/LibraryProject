32d1fb5321b4395881e1dc01fe318707ec13f4e9
Aleksander Dobija <aledob1324@gmail.com>
2025-01-15 22:55:24 +0100

    Create interface for register module

---
+++ interfaces/UserRegister.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.interfaces
+{
+    internal interface UserRegister
+    {
+
+        void RegisterUser(string userName, string password);
+
+        void Success();
+    }
+}
