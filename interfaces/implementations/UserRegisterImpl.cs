1883d90359b248103fa8a9e4369ab09a84cb3d32
Aleksander Dobija <aledob1324@gmail.com>
2025-01-20 23:36:24 +0100

    Adjust code to serialize users into json valid list with checker if username is taken

--- interfaces/implementations/UserRegisterImpl.cs
+++ interfaces/implementations/UserRegisterImpl.cs
@@ -3,26 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using LibraryProject.classes;
+using LibraryProject.controllers;
+using LibraryProject.exceptions;
+using LibraryProject.libraries;
 
 namespace LibraryProject.interfaces.implementations
 {
     internal class UserRegisterImpl : UserRegister
     {
         private string UserDB = @"../../../libraries/UserDB.json";
+
         public async void RegisterUser(string userName, string password)
         {
+
+            List<User> listOfExistingUsers = await UserController.FetchUsers();
+
+            //Console.WriteLine($"Number of users fetched: {listOfExistingUsers.Count}");
+
+            string validUsername = UsernameValidator.validateUsername(listOfExistingUsers, userName);
             
 
-            User user = new User(userName, password);
+            User user = new User(validUsername, password);
+
+            listOfExistingUsers.Add(user);
 
             try
             {
-
-                using (StreamWriter file = new StreamWriter(UserDB, true))
+                using (StreamWriter file = new StreamWriter(UserDB, false))
                 {
-                    string json = JsonSerializer.Serialize(user, new JsonSerializerOptions { WriteIndented = true }); // Serializacja do JSON
+                    
+                    //listOfExistingUsers.Add(user);
+                    //Console.WriteLine(user.getUserName());
+                    string json = JsonSerializer.Serialize(listOfExistingUsers, new JsonSerializerOptions() { WriteIndented = true, PropertyNamingPolicy = JsonNamingPolicy.CamelCase }); // Serializacja do JSON
+                    //Console.WriteLine(json);
+                    
                     await file.WriteAsync(json);
                 }
                 Success();
@@ -31,6 +48,19 @@ namespace LibraryProject.interfaces.implementations
             { 
             Console.WriteLine("UserRegisterImpl fail: file UserDB.json not found");
             }
+        }
+
+        public static void StartRegister()
+        {
+            UserController userController = new UserController();
+
+            Console.WriteLine("Create your account!");
+            Console.WriteLine("Insert username");
+            string username = Console.ReadLine();
+            Console.WriteLine("Insert password");
+            string password = Console.ReadLine();
+
+            userController.RegisterUser(username, password);
         }
 
         public void Success()
