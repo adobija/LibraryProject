92ae84a5005103d8c096a265c1bbb0d93bbcbb69
Aleksander Dobija <aledob1324@gmail.com>
2025-01-21 00:34:28 +0100

    Create new custom exception

---
+++ exceptions/UserDoesNotExistException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.exceptions
+{
+    internal class UserDoesNotExistException : Exception
+    {
+
+        public UserDoesNotExistException() : base("User with that username does not exist!")
+        {
+
+        }
+
+        public UserDoesNotExistException(string message) : base(message)
+        {
+
+        }
+    }
+}
