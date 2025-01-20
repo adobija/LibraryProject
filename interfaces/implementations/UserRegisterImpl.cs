e488f02195358c6417d6c3cdc42b49627fcf0e69
Aleksander Dobija <aledob1324@gmail.com>
2025-01-20 20:05:01 +0100

    Upgrade saving from simple text to json

--- interfaces/implementations/UserRegisterImpl.cs
+++ interfaces/implementations/UserRegisterImpl.cs
@@ -2,28 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using LibraryProject.classes;
 
 namespace LibraryProject.interfaces.implementations
 {
-    internal class UserRegisterImpl :UserRegister
+    internal class UserRegisterImpl : UserRegister
     {
-        public void RegisterUser(string userName, string password)
+        private string UserDB = @"../../../libraries/UserDB.json";
+        public async void RegisterUser(string userName, string password)
         {
+            
+
             User user = new User(userName, password);
 
-            string pathToUserDB = "../../../libraries/UserDB.txt";
             try
             {
-                using (StreamWriter file = new StreamWriter(pathToUserDB, true)) {
-                    file.WriteLine(user.ToString());
+
+                using (StreamWriter file = new StreamWriter(UserDB, true))
+                {
+                    string json = JsonSerializer.Serialize(user, new JsonSerializerOptions { WriteIndented = true }); // Serializacja do JSON
+                    await file.WriteAsync(json);
                 }
                 Success();
             }
             catch (FileNotFoundException e)
             { 
-            Console.WriteLine("UserRegisterImpl fail: file UserDB.txt not found");
+            Console.WriteLine("UserRegisterImpl fail: file UserDB.json not found");
             }
         }
 
