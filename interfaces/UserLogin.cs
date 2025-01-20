b36bb5152e13dfe6aa4226d3ea18cd0741c1fe59
Aleksander Dobija <aledob1324@gmail.com>
2025-01-21 00:14:14 +0100

    Create interface for login

--- interfaces/UserLogin.cs
+++ interfaces/UserLogin.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibraryProject.classes;
 
 namespace LibraryProject.interfaces
 {
     internal interface UserLogin
     {
-       void Login();
+        static abstract Task<User> Login();
     }
 }
