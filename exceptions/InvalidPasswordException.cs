aa509d112751804dd4b5099388caac65bb590474
Aleksander Dobija <aledob1324@gmail.com>
2025-01-21 00:28:06 +0100

    Update

--- exceptions/InvalidPasswordException.cs
+++ exceptions/InvalidPasswordException.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace LibraryProject.exceptions
 {
-    internal class InvalidPasswordException
+    internal class InvalidPasswordException : Exception
     {
+        public InvalidPasswordException() : base("Inserted 3 times incorrect password... Exiting the application...")
+        {
+
+        }
+
+        public InvalidPasswordException(string message) : base(message)
+        {
+
+        }
     }
 }
