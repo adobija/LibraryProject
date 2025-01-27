101062afa095cb6f533815eaf23c7de8ddbf7125
JCode1111 <kruzlakjakub6@gmail.com>
2025-01-27 18:35:34 +0100

    change UsersController

--- controllers/UserController.cs
+++ controllers/UserController.cs
@@ -13,20 +13,21 @@ namespace LibraryProject.controllers
     internal class UserController : UserRegisterImpl
     {
         private static string pathToUsersDB = "../../../libraries/UserDB.json";
-        public void Register(string userName, string password) {
+        public void Register(string userName, string password)
+        {
             RegisterUser(userName, password);
         }
 
-        
+
 
         public static async Task<List<User>> FetchUsers()
         {
-            
+
             try
             {
                 string json = File.ReadAllText(pathToUsersDB); // Wczytanie zawartości pliku
-                
-                var users = JsonSerializer.Deserialize<List<User>>(json, new JsonSerializerOptions {PropertyNamingPolicy = JsonNamingPolicy.CamelCase, WriteIndented = true});
+
+                var users = JsonSerializer.Deserialize<List<User>>(json, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase, WriteIndented = true });
 
                 return users;
 
@@ -41,18 +42,54 @@ namespace LibraryProject.controllers
 
         }
 
-        public static async Task<User> getUserAsync(string username) { 
+        public static async Task<User> getUserAsync(string username)
+        {
             List<User> users = await FetchUsers();
 
             User foundUser = null;
 
             foreach (User x in users)
             {
-                if (x.getUserName().ToUpper() == username.ToUpper()) { 
-                foundUser = x; break;
+                if (x.getUserName().ToUpper() == username.ToUpper())
+                {
+                    foundUser = x; break;
                 }
             }
             return foundUser;
         }
+
+
+        public static async Task SaveUpdatedUser(User updatedUser)
+        {
+            var users = await UserController.FetchUsers(); // Pobranie wszystkich użytkowników z pliku
+
+            // Znalezienie indeksu użytkownika, którego dane chcemy zaktualizować
+            var userIndex = users.FindIndex(u => u.userId == updatedUser.userId);
+
+            if (userIndex != -1)
+            {
+                // Aktualizacja danych użytkownika
+                users[userIndex] = updatedUser;
+            }
+            else
+            {
+                Console.WriteLine("Nie znaleziono użytkownika w bazie danych.");
+                return;
+            }
+
+            // Serializacja całej listy użytkowników z uwzględnieniem zmian
+            string json = JsonSerializer.Serialize(users, new JsonSerializerOptions { WriteIndented = true });
+
+            // Zapisanie całej listy użytkowników do pliku
+            await File.WriteAllTextAsync("../../../libraries/UserDB.json", json);
+        }
+
+
+
     }
+
+
+
+
 }
+
