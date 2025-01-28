492552306695f5e42e2c508a71529016447c00f9
Aleksander Dobija <aledob1324@gmail.com>
2025-01-28 19:15:05 +0100

    Create validator class for ISBN number

---
+++ libraries/ISBNValidator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryProject.classes;
+using LibraryProject.exceptions;
+
+namespace LibraryProject.libraries
+{
+    internal class ISBNValidator
+    {
+
+        public static bool isValid(long isbn) {
+            if (isbn < 1000000000000 || isbn > 9999999999999)
+            {
+                throw new InvalidISBN();
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static async Task<bool> isInDatabaseAsync(long isbn) {
+            List<DataBaseBook> dataBaseBooks = await LibrarySystem.LoadBooksAsync();
+            foreach (DataBaseBook book in dataBaseBooks) {
+                if (book.ISBN == isbn) { 
+                throw new ISBNAlreadyExistException();
+                }
+            }
+            return false;
+        }
+    }
+}
