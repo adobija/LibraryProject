ed420154f4f7d1fda93566051dd12ace41869538
Aleksander Dobija <aledob1324@gmail.com>
2025-01-28 19:14:43 +0100

    Create class for factory design pattern

---
+++ libraries/FactoryDesignLib.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryProject.abstracts;
+using LibraryProject.classes;
+using LibraryProject.classes.bookClasses;
+
+namespace LibraryProject.libraries
+{
+    internal class FactoryDesignLib
+    {
+        public static BookAbstract getBook(String type)
+        {
+            if (type.Equals("FANTASY"))
+            {
+                return new FantasyBook();
+            }
+            else if (type.Equals("MYSTERY"))
+            {
+                return new MysteryBook();
+            }
+            else if (type.Equals("ROMANCE"))
+            {
+                return new RomanceNovelBook();
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+    }
+}
