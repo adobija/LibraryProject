d642ae12d1a003a3979aa0e0307375f592742930
Aleksander Dobija <aledob1324@gmail.com>
2025-01-28 19:13:03 +0100

    Create new custom exceptions

---
+++ exceptions/ISBNAlreadyExistException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.exceptions
+{
+    internal class ISBNAlreadyExistException : Exception
+    {
+        public ISBNAlreadyExistException() : base("This ISBN already exist in database. Try Again!")
+        {
+
+        }
+
+        public ISBNAlreadyExistException(string message) : base(message)
+        {
+
+        }
+    }
+}
