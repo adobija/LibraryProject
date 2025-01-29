3faef29b89c46efe3471a3098460689e75160c9f
Aleksander Dobija <aledob1324@gmail.com>
2025-01-29 21:53:32 +0100

    Create new helper class

---
+++ libraries/ListPrint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryProject.classes;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace LibraryProject.libraries
+{
+    internal class ListPrint
+    {
+        public static void printList(List<DataBaseBook> books) {
+            int i = 0;
+            foreach (DataBaseBook book in books)
+            {
+                Console.Write($"{i}: ");
+                book.printDetailsOfBook();
+                Console.WriteLine();
+                i++;
+            }
+        }
+    }
+}
